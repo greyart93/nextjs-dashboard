b319d1c973fa8713f05a9178b2c078e866eb6ee3
saud <seussstaudinger@gmail.com>
2024-08-30 08:46:16 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <div><p>Customer Page</p></div>
+    );
+}
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,3 +1,5 @@
+'use cient';
+
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
